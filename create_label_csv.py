26366d79b4acb71fc3db529617475cf3540f965b
asinha4 <ankitasinha0811@gmail.com>
2021-05-29 09:02:49 +0530

    update model

---
+++ create_label_csv.py
@@ -0,0 +1,35 @@
+import os
+
+import pandas as pd
+
+os.chdir(r'HGM_data')
+
+folders = ['Below_CAM/A/', 'Below_CAM/B/', 'Below_CAM/C/', 'Below_CAM/D/', 'Below_CAM/E/', 'Below_CAM/F/',
+           'Below_CAM/G/', 'Below_CAM/H/', 'Below_CAM/I/', 'Below_CAM/J/', 'Below_CAM/K/', 'Below_CAM/L/',
+           'Below_CAM/M/', 'Below_CAM/N/', 'Below_CAM/O/', 'Below_CAM/P/', 'Below_CAM/Q/', 'Below_CAM/R/',
+           'Below_CAM/S/', 'Below_CAM/T/', 'Below_CAM/U/', 'Below_CAM/V/', 'Below_CAM/W/', 'Below_CAM/X/',
+           'Below_CAM/Y/', 'Below_CAM/Z/',
+           'Front_CAM/A/', 'Front_CAM/B/', 'Front_CAM/C/', 'Front_CAM/D/', 'Front_CAM/E/', 'Front_CAM/F/',
+           'Front_CAM/G/', 'Front_CAM/H/', 'Front_CAM/I/', 'Front_CAM/J/', 'Front_CAM/K/', 'Front_CAM/L/',
+           'Front_CAM/M/', 'Front_CAM/N/', 'Front_CAM/O/', 'Front_CAM/P/', 'Front_CAM/Q/', 'Front_CAM/R/',
+           'Front_CAM/S/', 'Front_CAM/T/', 'Front_CAM/U/', 'Front_CAM/V/', 'Front_CAM/W/', 'Front_CAM/X/',
+           'Front_CAM/Y/', 'Front_CAM/Z/',
+           'Left_CAM/A/', 'Left_CAM/B/', 'Left_CAM/C/', 'Left_CAM/D/', 'Left_CAM/E/', 'Left_CAM/F/', 'Left_CAM/G/',
+           'Left_CAM/H/', 'Left_CAM/I/', 'Left_CAM/J/', 'Left_CAM/K/', 'Left_CAM/L/', 'Left_CAM/M/', 'Left_CAM/N/',
+           'Left_CAM/O/', 'Left_CAM/P/', 'Left_CAM/Q/', 'Left_CAM/R/', 'Left_CAM/S/', 'Left_CAM/T/', 'Left_CAM/U/',
+           'Left_CAM/V', 'Left_CAM/W/', 'Left_CAM/X', 'Left_CAM/Y/', 'Left_CAM/Z/',
+           'Right_CAM/A/', 'Right_CAM/B/', 'Right_CAM/C/', 'Right_CAM/D/', 'Right_CAM/E/', 'Right_CAM/F/',
+           'Right_CAM/G/', 'Right_CAM/H/', 'Right_CAM/I/', 'Right_CAM/J/', 'Right_CAM/K/', 'Right_CAM/L/',
+           'Right_CAM/M/', 'Right_CAM/N/', 'Right_CAM/O/', 'Right_CAM/P/', 'Right_CAM/Q/', 'Right_CAM/R/',
+           'Right_CAM/S/', 'Right_CAM/T/', 'Right_CAM/U/', 'Right_CAM/V/', 'Right_CAM/W', 'Right_CAM/X/',
+           'Right_CAM/Y/', 'Right_CAM/Z/']
+
+files = []
+
+for folder in folders:
+    for file in os.listdir(folder):
+        label = folder.split('/')[1]
+        files.append([folder + file, label])
+
+df = pd.DataFrame(files, columns=['files', 'target']).to_csv('labels.csv')
+# print(df.head())
---
+++ train.py
@@ -0,0 +1,84 @@
+import os
+
+import torch
+import torch.utils.tensorboard as tb
+
+from model import CNNClassifier, save_model, load_model
+from .utils import load_data, ConfusionMatrix
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+classifier = CNNClassifier().to(device)
+
+
+def train(args):
+    from os import path
+    model = CNNClassifier()
+    loss = torch.nn.CrossEntropyLoss()
+    train_logger, valid_logger = None, None
+    if args.log_dir is not None:
+        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train_cnn2'), flush_secs=1)
+        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid_cnn2'), flush_secs=1)
+
+    global_step_train = 0
+    # optimizer = torch.optim.SGD(model.parameters(), lr=0.015, momentum=0.9, nesterov=True)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.015, weight_decay=0.001)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=20)
+
+    if not os.path.exists('cnn.th'):
+        epoch = 150
+        path = '/Users/asinha4/kaggle/HandGestureRecognition/HGM_data'
+        valid_path = '/Users/asinha4/kaggle/HandGestureRecognition/HGM_data'
+        validloader = load_data(valid_path)
+        trainloader = load_data(path)
+        model.train()
+
+        for ep in range(epoch):
+            train_confusionMatrix = ConfusionMatrix()
+            valid_confusionMatrix = ConfusionMatrix()
+            for i, data in enumerate(trainloader, 0):
+                images, labels = data
+                frw = model(images)
+                train_confusionMatrix.add(frw.argmax(1), labels)
+                train_loss = loss(frw, labels)
+                optimizer.zero_grad()
+                train_loss.backward()
+                optimizer.step()
+                global_step_train += 1
+                train_logger.add_scalar("loss", train_loss, global_step=global_step_train)
+            print(f'Running epoch={ep} with accuracy on train data = {train_confusionMatrix.global_accuracy}')
+            train_logger.add_scalar("accuracy", train_confusionMatrix.global_accuracy, global_step=global_step_train)
+
+            for i, validdata in enumerate(validloader, 0):
+                images, labels = validdata
+                valid_confusionMatrix.add(model(images).argmax(1), labels)
+
+            print(f'Running epoch={ep} with accuracy on valid data = {valid_confusionMatrix.global_accuracy}')
+
+            valid_logger.add_scalar("accuracy", valid_confusionMatrix.global_accuracy, global_step=global_step_train)
+
+            train_logger.add_scalar('lr', optimizer.param_groups[0]['lr'], global_step=global_step_train)
+            scheduler.step(valid_confusionMatrix.global_accuracy)
+        model.eval()
+        save_model(model)
+    else:
+        model = load_model(model)
+
+
+def check():
+    model = CNNClassifier()
+    model.load_state_dict(torch.load('/Users/asinha4/UTAustin/cs342-file/homework3/homework/cnn.th'))
+    for param_tensor in model.state_dict():
+        print(param_tensor, "\t", model.state_dict()[param_tensor].type())
+    # print(model)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--log_dir')
+    # Put custom arguments here
+
+    args = parser.parse_args()
+    train(args)
