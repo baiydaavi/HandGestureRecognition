b9d2b27427afc76d098a7dd02439b41c5ca8e4d9
Avinash <baidyaavinash@gmail.com>
2021-07-04 16:48:34 -0700

    Add separate normalization to x and y coordinates of the landmarks.
    The model is finally working well for all the signs and it is not confusing between similar looking signs.

--- gesture_detect.py
+++ gesture_detect.py
@@ -1,37 +1,14 @@
-import cv2
-import mediapipe as mp
 import time
 import pickle
 
 import numpy as np
 
+import cv2
+import mediapipe as mp
+
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 
-class DNN_Landmark_Model(nn.Module):
-  def __init__(self):
-    super().__init__()
-    self.hidden1 = nn.Linear(42, 256)
-    self.hidden2 = nn.Linear(256, 128)
-    self.hidden3 = nn.Linear(128, 64)
-    self.output = nn.Linear(64, 28)
-    self.dropout1 = nn.Dropout(0.25)
-    self.dropout2 = nn.Dropout(0.5)
-
-  def forward(self, x):
-    x = self.hidden1(x)
-    x = F.relu(x)
-    x = self.dropout2(x)
-    x = self.hidden2(x)
-    x = F.relu(x)
-    x = self.dropout2(x)
-    x = self.hidden3(x)
-    x = F.relu(x)
-    x = self.dropout1(x)
-    x = self.output(x)
-    return x
-
+from model import DNN_Landmark_Model
 
 class handDetector():
 
@@ -53,16 +30,15 @@ class handDetector():
         # deep network landmark model
         self.model = DNN_Landmark_Model()
         self.model.load_state_dict(torch.load(
-            'trained_models/DNN_landmarks_model.pth'))
+            'trained_models/Normalized_DNN_landmarks_model.pth'))
         self.model.eval()
 
         # logistic regression landmark model
-        #self.model = pickle.load(open('LogReg_landmarks_model.sav', 'rb'))
+        # self.model = pickle.load(open('LogReg_landmarks_model.sav', 'rb'))
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-        # print(results.multi_hand_landmarks)
 
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
@@ -97,7 +73,7 @@ class handDetector():
             if draw:
                 cv2.rectangle(img, (xmin - 80, ymin - 80), (xmax + 80,
                                                             ymax + 80),
-                (0, 255, 0), 2)
+                              (0, 255, 0), 2)
 
         return img
 
@@ -119,12 +95,18 @@ class handDetector():
                 lm_array.append(lm.y - ref_y)
             lm_array = np.array(lm_array)
 
+            lm_array[0::2] = (lm_array[0::2] - np.min(lm_array[0::2])) / (
+                        np.max(
+                            lm_array[0::2]) - np.min(lm_array[0::2]))
+            lm_array[1::2] = (lm_array[1::2] - np.min(lm_array[1::2])) / (
+                    np.max(
+                        lm_array[1::2]) - np.min(lm_array[1::2]))
             # deep network landmark model
             output = self.model(torch.tensor(lm_array).type(torch.FloatTensor))
             gesture_id = np.argmax(output.detach().numpy())
 
             # logistic regression landmark model
-            #gesture_id = self.model.predict(lm_array.reshape(1,-1))[0]
+            # gesture_id = self.model.predict(lm_array.reshape(1,-1))[0]
 
             gesture = self.labels[gesture_id]
 
--- model.py
+++ model.py
@@ -1,6 +1,30 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
+class DNN_Landmark_Model(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.hidden1 = nn.Linear(42, 256)
+        self.hidden2 = nn.Linear(256, 128)
+        self.hidden3 = nn.Linear(128, 64)
+        self.output = nn.Linear(64, 28)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.hidden1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.hidden2(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.hidden3(x)
+        x = F.relu(x)
+        x = self.dropout1(x)
+        x = self.output(x)
+        return x
 
 class CNNClassifier(torch.nn.Module):
 
