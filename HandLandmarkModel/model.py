a5e1e922f9553b275b9107efe3d1d017f529afe8
asinha4 <ankitasinha0811@gmail.com>
2021-07-20 17:39:42 +0530

    code cleanup
    1. merged code with main branch
    2. cleaned up code
    3. added training ipython file
    4. added trained model to /cnn

--- HandLandmarkModel/model.py
+++ HandLandmarkModel/model.py
@@ -1,6 +1,8 @@
-import torch
+import torch.nn
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
+
 
 class DNN_Landmark_Model(nn.Module):
 
@@ -25,3 +27,36 @@ class DNN_Landmark_Model(nn.Module):
         x = self.dropout1(x)
         x = self.output(x)
         return x
+
+
+class CNN(torch.nn.Module):
+    def __init__(self, backbone='mobilenet_v2'):
+        self.backbone = backbone
+
+        addLayers = False
+        if backbone == 'resnet50':
+            model = models.resnet50(pretrained=True)
+            for param in model.parameters():
+                param.requires_grad = False
+            if addLayers:
+                model.fc = torch.nn.Sequential(torch.nn.Linear(2048, 1024),
+                                               torch.nn.ReLU(),
+                                               torch.nn.Dropout(0.2),
+                                               torch.nn.Linear(1024, 29),
+                                               torch.nn.LogSoftmax(dim=1)
+                                               )
+            else:
+                model.fc = torch.nn.Linear(2048, 29)
+        elif backbone == 'mobilenet_v2':
+            model = models.mobilenet_v2(pretrained=True)
+            for param in model.parameters():
+                param.requires_grad = False
+            if addLayers:
+                model.classifier = torch.nn.Sequential(torch.nn.Linear(1280, 1024),
+                                                       torch.nn.ReLU(),
+                                                       torch.nn.Dropout(0.2),
+                                                       torch.nn.Linear(1024, 29),
+                                                       torch.nn.LogSoftmax(dim=1)
+                                                       )
+            else:
+                model.classifier = torch.nn.Linear(1280, 29)
